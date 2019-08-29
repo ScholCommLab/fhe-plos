0ebe49589d928f15dffa239b212591cd1b3d8232
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2019-08-28 17:23:28 -0700

    added download script

---
+++ download_dataset.sh
@@ -0,0 +1,5 @@
+wget -O "data/external/PLOS_2015-2017_idArt-DOI-PY-Journal-Title-LargerDiscipline-Discipline-Specialty.csv"  https://dataverse.harvard.edu/api/access/datafile/3550479 
+wget -O "data/input/altmetric_counts.csv"  https://dataverse.harvard.edu/api/access/datafile/3550456  
+wget -O "data/input/graph_api_counts.csv" https://dataverse.harvard.edu/api/access/datafile/3550457 
+wget -O "data/input/plos_one_articles.csv"  https://dataverse.harvard.edu/api/access/datafile/3550458
+wget -O "data/input/query_details.csv"  https://dataverse.harvard.edu/api/access/datafile/3550459 
